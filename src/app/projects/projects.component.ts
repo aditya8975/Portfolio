86f6d0c0fd8acd1c53e53361629926ce734509d2
ABK <98167690+aditya8975@users.noreply.github.com>
2026-02-11 16:48:56 +0530

    Update projects.component.ts

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,13 +13,13 @@ export class ProjectsComponent implements OnInit {
       title: 'akManager',
       technologies: 'TypeScript , BatchFile',
       description: [
-        'akmanager - The Fastest JS Package Manager Installs packages 10x faster using prebuilt cache and Bun for instant setups. ',
+        'akmanager - The Fastest JS Package Manager Installs packages 10x faster using prebuilt cache and Bun for instant setups.',
         'akmanager speeds up package installations by:',
-️' Using Bun as the backend - Faster than npm & yarn',
+️'Using Bun as the backend - Faster than npm & yarn',
   ️'Preloading Popular Packages - Avoids network downloads',
-️' Parallel Processing - Installs multiple packages simultaneously',
-️' Smart Caching - Installs from a local store instead of fetching from the internet',
-️' Compressed Prebuilt Package Cache - Extracts prebuilt dependencies for instant setup',
+️'Parallel Processing - Installs multiple packages simultaneously',
+️'Smart Caching - Installs from a local store instead of fetching from the internet',
+️'Compressed Prebuilt Package Cache - Extracts prebuilt dependencies for instant setup',
         
       ],
       link:'https://akmanager-site.vercel.app/'
@@ -30,7 +30,7 @@ export class ProjectsComponent implements OnInit {
       title: 'Chess',
       technologies: 'Backend: Node.js, Express, Socket.io Frontend: HTML, CSS (Tailwind), JavaScript Chess Logic: Chess.js',
       description: [
-        'Worked on Socket Programing to create chess game ',
+        'Worked on Socket Programing to create chess game',
         'Learned Socket Programing and React',
         
       ],
@@ -41,21 +41,21 @@ export class ProjectsComponent implements OnInit {
       title: 'Akomponent-lib',
       technologies: 'Js ',
       description: [
-        'AKomponents Library is a lightweight, customizable React UI library with modern, responsive components. ',
+        'AKomponents Library is a lightweight, customizable React UI library with modern, responsive components.',
         'Designed for performance and ease of use, it includes buttons, cards, forms, navigation, and more',
-        'Prebuilt, Reusable UI Components ',
-' Fully Customizable & Responsive ',
-'Optimized for Performance & Accessibility ',
+        'Prebuilt, Reusable UI Components',
+'Fully Customizable & Responsive',
+'Optimized for Performance & Accessibility',
 'Works Seamlessly with React & Tailwind CSS',
-' Minimal Dependencies for Faster Load Times ',
+'Minimal Dependencies for Faster Load Times',
       ],
       link:'https://akomponent-site.vercel.app/'
     },
      {
       title: 'e-commerce',
-      technologies: ' Angular, Angular Material',
+      technologies: 'Angular, Angular Material',
       description: [
-        'Worked to build an e-commerce application using angular ',
+        'Worked to build an e-commerce application using angular',
         'Got to learn about Angular Material and Services',
         'Learned Typescript to build an webapp.',
       ],
